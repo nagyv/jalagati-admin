3200b41be3abb105d7336a3abafd933cbe816b65
Viktor Nagy <viktor.nagy@gmail.com>
2015-01-21 21:24:50 +0100

    improved gulp with release command

--- gulp/server.js
+++ gulp/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-
+var insert = require('gulp-insert');
 var util = require('util');
 
 var browserSync = require('browser-sync');
@@ -32,7 +32,23 @@ function browserSyncInit(baseDir, files, browser) {
 
 }
 
-gulp.task('serve', ['watch'], function () {
+gulp.task('inject-dist-server', function(){
+  return gulp.src('src/app/app.js')
+    .pipe(insert.transform(function(contents) {
+      return contents.replace('http://127.0.0.1:8000', 'https://jogaadmin.herokuapp.com');
+    }))
+    .pipe(gulp.dest('src/app/'));
+});
+
+gulp.task('inject-server', function(){
+  return gulp.src('src/app/app.js')
+    .pipe(insert.transform(function(contents) {
+      return contents.replace('https://jogaadmin.herokuapp.com', 'http://127.0.0.1:8000');
+    }))
+    .pipe(gulp.dest('src/app/'));
+});
+
+gulp.task('serve', ['inject-server', 'watch'], function () {
   browserSyncInit([
     'src',
     '.tmp'
